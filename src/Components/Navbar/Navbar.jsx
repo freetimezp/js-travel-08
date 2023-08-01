1de03df55a51014628f693b9f2a2925920509f92
freetimezp <kraftukrnet@ukr.net>
2023-08-02 01:33:05 +0300

    03. js-travel-08. Navbar Component. Show, hide menu.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { BiLogoMediumOld } from 'react-icons/bi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { PiDotsNineBold } from 'react-icons/pi';
 
 import './Navbar.css';
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState('menu');
+
+    const showNavbar = () => {
+        setNavbar('menu showNavbar');
+    }
+
+    const removeNavbar = () => {
+        setNavbar('menu');
+    }
+
     return (
-        <div>
-            Navbar
+        <div className='navbar'>
+            <div className="logoDiv">
+                <BiLogoMediumOld className="icon" />
+                <span>Dream Travel</span>
+            </div>
+
+            <div className={navbar}>
+                <ul>
+                    <li className="nav-list">
+                        Destination
+                    </li>
+                    <li className="nav-list">
+                        About Us
+                    </li>
+                    <li className="nav-list">
+                        Testimonial
+                    </li>
+                    <li className="nav-list">
+                        Gallery
+                    </li>
+                </ul>
+
+                <AiFillCloseCircle className='icon closeIcon' onClick={removeNavbar} />
+            </div>
+
+            <button className="signUpBtn btn">
+                Sign Up
+            </button>
+
+            <PiDotsNineBold className='icon menuIcon' onClick={showNavbar} />
         </div>
     );
 };
