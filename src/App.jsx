1892bbb52518b57533dc5f494b0cf057cbc26ac8
freetimezp <kraftukrnet@ukr.net>
2023-08-02 22:54:31 +0300

    05. js-travel-08. Middle Component.

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
     <div>
       <Navbar />
       <Home />
-      {/* <Middle />
-      <Destinations />
+      <Middle />
+      {/* <Destinations />
       <Portfolio />
       <Reviews />
       <Questions />
--- src/Components/Middle/Middle.jsx
+++ src/Components/Middle/Middle.jsx
@@ -4,8 +4,27 @@ import './Middle.css';
 
 const Middle = () => {
     return (
-        <div>
-            Middle
+        <div className='middle section'>
+            <div className="secContainer container">
+                <div className="grid">
+                    <span className="flex">
+                        <h1>10</h1>
+                        <p>World Of Experience</p>
+                    </span>
+                    <span className="flex">
+                        <h1>2k+</h1>
+                        <p>Fine Destinations</p>
+                    </span>
+                    <span className="flex">
+                        <h1>10k+</h1>
+                        <p>Customer Reviews</p>
+                    </span>
+                    <span className="flex">
+                        <h1>4.8</h1>
+                        <p>Overall Rating</p>
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
