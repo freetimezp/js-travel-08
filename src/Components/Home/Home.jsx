bea2f574d80dc795a1eb59911b1d4af5d1ecd658
freetimezp <kraftukrnet@ukr.net>
2023-08-05 01:02:36 +0300

    12. js-travel-08. Navbar, Home. Responsive.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,6 +4,10 @@ import './Home.css';
 import { AiOutlineSwapRight } from 'react-icons/ai';
 
 import Video from '../../Assets/home.mp4';
+import placeImage1 from '../../Assets/place-1.jpg';
+import placeImage2 from '../../Assets/place-2.jpg';
+import placeImage3 from '../../Assets/place-3.jpg';
+import placeImage4 from '../../Assets/place-4.jpg';
 
 const Home = () => {
     return (
@@ -28,10 +32,10 @@ const Home = () => {
                 <div className="content">
                     <h3>Popular Places</h3>
                     <div className="images flex">
-                        {/* <img src="" alt="place" />
-                        <img src="" alt="place" />
-                        <img src="" alt="place" />
-                        <img src="" alt="place" /> */}
+                        <img src={placeImage1} alt="place" />
+                        <img src={placeImage2} alt="place" />
+                        <img src={placeImage3} alt="place" />
+                        <img src={placeImage4} alt="place" />
                     </div>
                 </div>
             </div>
